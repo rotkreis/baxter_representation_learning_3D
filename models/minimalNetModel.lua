b1ecb6426ac42a6133ea3b91b41ffeec41d17ee2
Natalia Diaz Rodriguez <diaz.rodriguez.natalia@gmail.com>
2017-04-23 22:24:00 +0200

    minimal example for cpu only runs including minimal network, augmenting size RAM per epock by splitting the dataset in more parts in order to load less image into RAM at one time with nb_part

--- models/minimalNetModel.lua
+++ models/minimalNetModel.lua
@@ -7,8 +7,9 @@ function getModel()
 	nbFilter=1
 	input = nn.Identity()()
 	conv1 = nn.SpatialConvolution(3, nbFilter, 3, 3)(input)
-	View_1=nn.View(1198198)(conv1)
-	out=nn.ReLU()(nn.Linear(1198198, 3)(View_1))
+	--1*198*198, (one feature Map of 200*200, reduced to 198*198 by padding of convolution):
+	View_1=nn.View(1*198*198)(conv1)
+	out=nn.ReLU()(nn.Linear(1*198*198, 3)(View_1))
 	gmod = nn.gModule({input}, {out})
 	local method = 'xavier'
 	local gmod = require('weight-init')(gmod, method)
