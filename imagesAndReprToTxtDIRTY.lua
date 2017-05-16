c79447eaeddc22c316c6fa45de5d9ac55282a4c8
Mathieu-Seurin <matseurin@cegetel.com>
2017-05-16 14:43:12 +0200

    Commiting script to generate images and repr, name of txt changed

---
+++ imagesAndReprToTxtDIRTY.lua
@@ -0,0 +1,38 @@
+require 'lfs'
+require 'torch'
+require 'image'
+require 'nn'
+require 'nngraph'
+require 'cunn'
+
+local imagesFolder = 'simpleData3D'
+local modelString = 'dummyModelForDavid.t7'
+
+local model = torch.load(modelString)
+outStr = ''
+
+for seqStr in lfs.dir(imagesFolder) do
+   if string.find(seqStr,'record') then
+      local imagesPath = imagesFolder..'/'..seqStr..'/recorded_cameras_head_camera_2_image_compressed'
+      for imageStr in lfs.dir(imagesPath) do
+         if string.find(imageStr,'jpg') then
+            local fullImagesPath = imagesPath..'/'..imageStr
+            local reprStr = ''
+            local img=image.load(fullImagesPath,3,'float')
+
+            img=image.scale(img,'200x200'):double()
+            repr = model:forward(img)
+
+            for i=1,repr:size(1) do
+               reprStr = reprStr..repr[i]..' '
+            end
+
+            outStr = outStr..fullImagesPath..' '..reprStr..'\n'
+            
+         end
+      end
+   end
+end        
+        
+file = io.open('saveImagesAndRepr.txt', 'w')
+file:write(outStr)
