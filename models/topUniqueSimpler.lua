930cf2bea4a5b664df0a118acf8e403facf45307
Mathieu-Seurin <matseurin@cegetel.com>
2017-05-17 11:31:05 +0200

    Forgot new model

---
+++ models/topUniqueSimpler.lua
@@ -0,0 +1,48 @@
+require 'nn'
+
+-- network-------------------------------------------------------
+function getModel(Dimension)
+	nbFilter=32
+
+	Timnet = nn.Sequential()
+
+	Timnet:add(nn.SpatialConvolution(3, nbFilter, 3, 3))
+	Timnet:add(nn.SpatialBatchNormalization(nbFilter))
+	Timnet:add(nn.ReLU())
+        Timnet:add(nn.SpatialMaxPooling(2,2,2,2))
+
+
+	Timnet:add(nn.SpatialConvolution(nbFilter, 2*nbFilter, 3, 3))
+	Timnet:add(nn.SpatialBatchNormalization(2*nbFilter))
+	Timnet:add(nn.ReLU())
+        Timnet:add(nn.SpatialMaxPooling(2,2,2,2))
+
+	Timnet:add(nn.SpatialConvolution(2*nbFilter, 4*nbFilter, 3, 3))
+	Timnet:add(nn.SpatialBatchNormalization(4*nbFilter))
+	Timnet:add(nn.ReLU())
+	Timnet:add(nn.SpatialMaxPooling(2,2,2,2))
+
+	Timnet:add(nn.SpatialConvolution(4*nbFilter, 8*nbFilter, 3, 3))
+	Timnet:add(nn.SpatialBatchNormalization(8*nbFilter))
+	Timnet:add(nn.ReLU())
+	Timnet:add(nn.SpatialMaxPooling(2,2,2,2))
+
+	Timnet:add(nn.SpatialConvolution(8*nbFilter, Dimension, 1, 1))
+	Timnet:add(nn.SpatialBatchNormalization(Dimension))
+	Timnet:add(nn.ReLU())
+
+        -- If image are 200x200 => View(3*10*10):setNumInputDims(3) also works
+	Timnet:add(nn.View(-1):setNumInputDims(3))
+	Timnet:add(nn.Linear(300, 500)) -- 300 is for 3*3*10
+	Timnet:add(nn.ReLU())
+	Timnet:add(nn.Linear(500, 100))
+	Timnet:add(nn.ReLU())
+	Timnet:add(nn.Linear(100, Dimension))
+	Timnet:add(nn.HardTanh())
+
+	-- Initiallisation : "Understanding the difficulty of training deep feedforward neural networks"
+	local method = 'xavier'
+	local Timnet = require('weight-init')(Timnet, method)
+	print('Timnet\n' .. Timnet:__tostring());
+	return Timnet
+end
